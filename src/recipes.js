be948a1c3f5a0cf297278f95383d4e9c13c2fc6c
SILVESTR0N <115602954+SILVESTR0N@users.noreply.github.com>
2024-11-23 14:37:45 -0500

    Update recipes.js

--- src/recipes.js
+++ src/recipes.js
@@ -1,1 +1,23 @@
+const { createClient } = require('@supabase/supabase-js')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const domain = process.env.SUPABASE_DOMAIN
+const supabase_public_key = process.env.SUPABASE_PUBLIC_KEY
+
+const supabase = createClient(domain, supabase_public_key)
+
+const spoon_key = process.env.SPOONACULAR_KEY
+
+async function trackRecipe(recipe){
+  return
+}
+
+async function searchRecipe(cuisine, time, diet, main_ingredient){
+  return
+}
+
+async function addRecipe(cuisine, time, diet, main_ingredient){
+  return
+}
 
